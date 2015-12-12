91c6cc94cb2d558a4b87598a44ab3e25c89964fd
satokifuruya <Satoki@FS-no-MacBook-Air.local>
2015-12-12 12:31:16 +0900

    user#showの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+    @nickname = User.find(params[:id]).nickname
+    @tweets = User.find(params[:id]).tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
